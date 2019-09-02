8fbf28e926187b754cad4d7e40eecb68e622782a
sampathkumar <semsampathkumar@gmail.com>
2019-09-02 17:20:08 +0530

    for loops 2

---
+++ whileloops/forloops2.js
@@ -0,0 +1,9 @@
+console.log("counting the numbers")
+for(var i=0;i < 19; i+=15){
+    console.log(i);
+}
+console.log("counting the string")
+var str ="jiknjdkika";
+for(var i=1;i < str.length; i+=2){
+    console.log(str[i]);
+}
