aa3e2c141aa5b7dbf33fc70bf51d643616b45846
sampathkumar <semsampathkumar@gmail.com>
2019-09-05 17:04:37 +0530

    array iteration for loops

---
+++ array iteration/forloop.js
@@ -0,0 +1,11 @@
+ var lap = ["lenovo", "apple", "hp", "dell", "readme"];
+ console.log(lap[0])
+ console.log(lap[4])
+
+ for (var i=1; i < lap.length; i++){
+     console.log(lap[i])
+ }
+
+ for (var i=1; i < lap.length; i++){
+     alert(lap[i])
+}
